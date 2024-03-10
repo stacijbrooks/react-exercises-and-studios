3aacb907de8a4a5710a015a244a0a2db8a1fb768
stacijbrooks <stacibrooksfitness@gmail.com>
2024-03-10 18:58:38 -0500

    React Exercises and Studio

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let books = "shrug";
+   let book1 = "https://m.media-amazon.com/images/I/41LavpbFAzL._SY445_SX342_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/61ZWycJRKEL._SY466_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/41au-1JYjWL._SX342_SY445_.jpg";
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <h3>{books}</h3>
+         <img src={book1} alt="How Dogs Love Us" />
+         <img src={book2} alt="The Complete Book of Dogs" />
+         <img src={book3} alt="The Art of Training Your Dog" />
       </div>      
    );
 }
