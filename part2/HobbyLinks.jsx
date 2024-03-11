b5b2aad2972f191f4d5c519670796934902bc4ae
stacijbrooks <stacibrooksfitness@gmail.com>
2024-03-10 19:33:01 -0500

    IDK

---
+++ part2/HobbyLinks.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+    
+<body>
+    <h2>My Favorite Hobby Websites</h2>
+    <ul>
+        <li><a href="https://www.ilovekickboxing.com/">Kickboxing Website</a></li>
+        <li><a href="https://www.codecademy.com/">Coding Website</a></li>
+        <li><a href="https://westinnkennels.com/training/">Dog Training Website</a></li>
+    </ul> 
+<div className="App">
+    <MovieList />
+    <ChoresList />
+    <BookList />
+    <HobbyLinks />
+    </div>
+</body>
+
+
+export default HobbyLinks;
