236f2a929c56970c834df6ef9503a7d5c893856e
stacijbrooks <stacibrooksfitness@gmail.com>
2024-03-16 16:01:38 -0500

    React Part 2 Exercise

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -2,11 +2,20 @@ import './styles.css';
 
 function Button() {
 
+   function onLearnMore() {
+      alert("Splish Splash");
+   }
+
    return ( 
-         <button>
+         <button> onClick={onLearnMore}>
             Learn More
          </button>
+//The onClick attribute in this code snippet is used to assign the onLearnMore function as the event handler 
+//for the click event on the button element. When the button is clicked, the onLearnMore function will be executed, 
+//and an alert with the message "Splish Splash!" will be displayed.
    );
 }
+
+
 
 export default Button;
--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,23 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+let listItem = oceans.map(ocean =>
+   (
+      <div key={ocean.id} className="profile">
+         <img src={ocean.image} alt={ocean.name} className="img" />
+      </div>
+   )
+);
 
-function Profile()
-{
-   return(
+function Profile() {
+   return (
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+         <ul>
+            {listItem}
+         </ul>
          <Button />
-      </>  
+      </>
    );
 }
 
